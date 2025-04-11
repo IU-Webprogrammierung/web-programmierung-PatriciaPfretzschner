dc12c30e5c731433a4891aee4ddee5991160696c
Patricia Pfretzschner <patricia.pfretzschner@iu-study.org>
2025-04-11 13:04:42 +0200

    Bildern einen border-radius hinzugefügt, "Newsletter abonnieren" Bereicht hinzugefügt mit Logik-Abfrage (javascript) und Erfolgsmeldung, Absprünge unter meine Leidenschaft (index) mit Schatten versehen

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     const popup = document.getElementById('cookiePopup');
     const button = document.getElementById('acceptCookies');
-  
+
 
     if (!sessionStorage.getItem('cookieAccepted')) {
-      popup.classList.remove('hidden');
+        popup.classList.remove('hidden');
     }
-  
+
     button.addEventListener('click', function () {
-      popup.classList.add('hidden');
-      sessionStorage.setItem('cookieAccepted', 'true');
+        popup.classList.add('hidden');
+        sessionStorage.setItem('cookieAccepted', 'true');
     });
-  });
-  
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('newsletterForm');
+    const successMessage = document.getElementById('successMessage');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        successMessage.classList.remove('hidden');
+        form.reset();
+    });
+});
